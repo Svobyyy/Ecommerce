8d22e9a33b82aceac7ecb6c11402a98b9efeec84
svoby <svobymates@gmail.com>
2023-12-11 03:10:01 +0100

    meta fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ export default function App({ Component, pageProps }: AppProps) {
         ></meta>
         <meta name="keywords" content="e-commerce,ecommerce,shop,market" />
         <meta name="robots" content="index, follow" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#242424" />
